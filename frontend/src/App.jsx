3a7503f06fea9c887f0c89c3afdb01144ae7b100
Senpai <clevercoder0307@gmail.com>
2025-12-05 14:22:35 +0530

    fixed fetching URL address

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function App() {
   const [message, setMessage] = useState("")
   useEffect(() => {
-    fetch("http://localhost:5500/api")
+    fetch("http://34.228.21.142:5500/api")
       .then((res) => res.json()).then((data)=> setMessage(data.message))
       .catch((err) => console.log(err));
   }, []);
